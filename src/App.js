d6f982f9562afc321ea7ffa62bbcab470953f4d3
kobekovich <d.v.kove@gmail.com>
2020-01-13 22:54:24 +0200

    A simple navbar created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Let's go</h1>
-    </div>
-  );
+
+class App extends Component {
+  render() {
+    return (
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="#home">NBA-daily</Navbar.Brand>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+          <Nav.Link href="#link1">Link1</Nav.Link>
+          <Nav.Link href="#link2">Link2</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    )
+  }
 }
 
 export default App;
