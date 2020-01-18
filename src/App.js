f8489a49f1e8c5a9eaf5738ce8bf44801006ea2b
kobekovich <d.v.kove@gmail.com>
2020-01-18 18:49:26 +0200

    Navbar modified

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Header/>
+        <Header />
         <Container>
           {cardsList}
         </Container>
--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,21 +1,29 @@
 import React, { Component } from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+//import NavDropdown from 'react-bootstrap/Dropdown';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import FormControl from 'react-bootstrap/FormControl';
 
 class Header extends Component {
     render() {
       return (
-        <Navbar bg="info" fixed="top">
+        <Navbar bg="info" fixed="top" expand="md">
           <Navbar.Brand href="#home" className="text-white">NBA-daily</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav" className="justify-content-center">
-            <Nav>
+            <Nav className="mr-auto">
               <Nav.Link href="#teams">Teams</Nav.Link>
               <Nav.Link href="#players">Players</Nav.Link>
               <Nav.Link href="#standing">Standing</Nav.Link>
               <Nav.Link href="#scores">Scores</Nav.Link>
               <Nav.Link href="#news">News</Nav.Link>
             </Nav>
+            <Form inline>
+                <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                <Button variant="outline-dark" className="my-2">Search</Button>
+            </Form>
           </Navbar.Collapse>
         </Navbar>
       )
